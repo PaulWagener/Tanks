d4756d76e28bff0b7ac3a5267b86bc5e72ad9922
Max Nijholt <m.nijholt@student.avans.nl>
2014-06-05 13:40:53 +0200

    Wall is always solid

--- src/tanks/model/Wall.java
+++ src/tanks/model/Wall.java
@@ -4,8 +4,8 @@ import java.awt.image.BufferedImage;
 
 public class Wall extends GameObject {
 
-	public Wall(BufferedImage img, int x, int y, int z, boolean s) {
-		super(img, x, y, z, s);
+	public Wall(BufferedImage img, int x, int y, int z) {
+		super(img, x, y, z, true);
 	}
-
+	
 }
