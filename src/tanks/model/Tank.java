29705792dda78d9d6bc7dc70d862b18ee33a8cc9
michael <man.schouten@student.avans.nl>
2014-06-05 14:07:55 +0200

    Tank class reload added

--- src/tanks/model/Tank.java
+++ src/tanks/model/Tank.java
@@ -4,10 +4,32 @@ import java.awt.image.BufferedImage;
 
 public class Tank extends GameObject
 {
-
-	public Tank(BufferedImage img, int x, int y, int z, boolean so, int sp)
+	private int health;
+	private int reload;
+	
+	public Tank(BufferedImage img, int x, int y, int z, int sp, int health)
 	{
-		super(img, x, y, z, so, sp);
+		super(img, x, y, z, true, sp);
+	}
+
+	public int getHealth()
+	{
+		return health;
+	}
+
+	public void setHealth(int health)
+	{
+		this.health = health;
+	}
+
+	public int getReload()
+	{
+		return reload;
+	}
+
+	public void setReload(int reload)
+	{
+		this.reload = reload;
 	}
 
 }
