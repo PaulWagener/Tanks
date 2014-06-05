091d387a2af3797848cf4ead1866f1e4ebf89683
stanvanheumen <cjgheume@student.avans.nl>
2014-06-05 13:36:18 +0200

    - added the frame - added the main panel - added the gui package

---
+++ src/tanks/gui/FieldPanel.java
@@ -0,0 +1,14 @@
+package tanks.gui;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class FieldPanel extends JPanel {
+
+	public FieldPanel() {
+		setPreferredSize(new Dimension(600, 600));
+	}
+	
+}
---
+++ src/tanks/gui/TankFrame.java
@@ -0,0 +1,17 @@
+package tanks.gui;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class TankFrame extends JFrame {
+
+	public TankFrame(String title) {
+		setTitle(title);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setContentPane(new FieldPanel());
+		pack();
+		setVisible(true);
+	}
+	
+}
