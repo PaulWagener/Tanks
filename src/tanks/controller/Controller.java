cc847af77ecc9078185cdf087e2378a2c4059f17
stanvanheumen <cjgheume@student.avans.nl>
2014-06-05 14:14:41 +0200

    worked more on controller

--- src/tanks/controller/Controller.java
+++ src/tanks/controller/Controller.java
@@ -1,19 +1,35 @@
 package tanks.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import tanks.gui.FieldPanel;
+import tanks.model.Crate;
+import tanks.model.Projectile;
+import tanks.model.Tank;
+import tanks.model.Wall;
 
 public class Controller implements Runnable {
 	
 	private final int FPS = 60;
 	
-	private FieldPanel 				fieldPanel;
-	private Thread 					thread;
-	//private ArrayList<GameObject> 	objects;
+	private FieldPanel 			fieldPanel;
+	private Thread 				thread;
+	private Tank[]				tanks;
+	private List<Crate>			crates;
+	private List<Projectile>	projectiles;
+	private List<Wall>			walls;
 	
 	public Controller(FieldPanel fp) {
 		fieldPanel 	= fp;
 		thread 		= new Thread(this);
-		//objects		= new ArrayList<GameObject>();
+		tanks		= new Tank[] {
+				new Tank(null, 8, 8, 0, 10, 100), 
+				new Tank(null, 16, 16, 0, 10, 100)
+				};
+		crates		= new ArrayList<Crate>();
+		walls		= new ArrayList<Wall>();
+		projectiles	= new ArrayList<Projectile>();
 		thread.start();
 	}
 	
