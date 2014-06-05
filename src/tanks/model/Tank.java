8b34311490757ffe18306ed583b6c9ec5c030335
michael <man.schouten@student.avans.nl>
2014-06-05 13:56:04 +0200

    Tank class added extend of GameObject

---
+++ src/tanks/model/Tank.java
@@ -0,0 +1,13 @@
+package tanks.model;
+
+import java.awt.image.BufferedImage;
+
+public class Tank extends GameObject
+{
+
+	public Tank(BufferedImage img, int x, int y, int z, boolean so, int sp)
+	{
+		super(img, x, y, z, so, sp);
+	}
+
+}
